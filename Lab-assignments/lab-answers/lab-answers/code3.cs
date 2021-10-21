b78c7c6cee0c9ccf1c469081a7e4333e610aeec4
divyanshukumar10 <divyanshu.kr10@gmail.com>
2021-10-22 02:01:27 +0530

    code--3

---
+++ Lab-assignments/lab-answers/lab-answers/code3.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+namespace lab_answers
+{
+    public class code3
+    {
+        int rollno;
+        string name;
+        string dept;
+        int sem;
+        string branch;
+        int[] marks = new int[5];
+
+        void getmarks()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                marks[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        void display()
+        {
+            int sum = marks.Sum();
+            Array.Sort(marks);
+            float avg = sum / 5;
+            if (marks[4] < 35)
+            {
+                Console.WriteLine("FAIL");
+            }
+            else
+            {
+                if (avg < 50)
+                    Console.WriteLine("FAIL");
+                else
+                    Console.WriteLine("PASS");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            code3 student = new code3();
+            student.rollno = Convert.ToInt32(Console.ReadLine());
+            student.name = Console.ReadLine();
+            student.dept = Console.ReadLine();
+            student.sem = Convert.ToInt32(Console.ReadLine());
+            student.branch = Console.ReadLine();
+
+            student.getmarks();
+            student.display();
+
+        }
+    }
+}
