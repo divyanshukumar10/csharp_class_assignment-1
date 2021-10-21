7fd2d7a8902d7898e28dcc86b91efae50c98794b
divyanshukumar10 <divyanshu.kr10@gmail.com>
2021-10-22 00:57:37 +0530

    4and7

---
+++ Lab-assignments/lab-answers/lab-answers/code4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace lab_answers
+{
+    public class code4
+    {
+        int bn;
+        string bookname;
+        string booktitle;
+        string bookauthor;
+        int quantityofbooks;
+        int bookprice;
+
+        void display()
+        {
+            Console.Write("Book name= {0}\nBook Title= {1}\nBy= {2}\nQuantity= {3}\nBook Price= {4}\n", bn, bookname, booktitle, bookauthor, quantityofbooks, bookprice);
+        }
+        void calculate()
+        {
+            Console.Write("Total Bill Amount = {0}", quantityofbooks * bookprice);
+        }
+
+        static void Main(string[] args)
+        {
+            code4 book = new code4();
+            book.bn = Convert.ToInt32(Console.ReadLine());
+            book.bookname = Console.ReadLine();
+            book.booktitle = Console.ReadLine();
+            book.bookauthor = Console.ReadLine();
+            book.quantityofbooks = Convert.ToInt32(Console.ReadLine());
+            book.bookprice = Convert.ToInt32(Console.ReadLine());
+
+            book.display();
+            book.calculate();
+        }
+    }
+}
---
+++ Lab-assignments/lab-answers/lab-answers/code7.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace lab_answers
+
+{
+    public class code7
+    {
+            public int rollno;
+            public string name;
+            public int eng, maths, sci;
+
+            void display()
+            {
+                float total = eng + maths + sci;
+                float per = (total/300)*100;
+                Console.Write("Total marks = {0}\n", total);
+                Console.Write("Total percentage = {0}", per);
+            }
+        
+
+        static void Main(string[] args)
+        {
+            code7 user = new code7();
+            user.rollno = Convert.ToInt32(Console.ReadLine());
+            user.name = Console.ReadLine();
+            user.eng = Convert.ToInt32(Console.ReadLine());
+            user.maths = Convert.ToInt32(Console.ReadLine());
+            user.sci = Convert.ToInt32(Console.ReadLine());
+
+            user.display();
+        }
+
+    }
+}
